32e664c7e5847a2c999ad65cddda7a07a8bff10a
추민수 <devsoftychoo@gmail.com>
2023-08-14 18:07:57 +0900

    Design/ProfilePage
    profilepage listview UI 구현 및 필요한 xml style파일들 추가

--- app/src/main/java/com/example/nbcamp_week4_assignment/ProfileActivity.kt
+++ app/src/main/java/com/example/nbcamp_week4_assignment/ProfileActivity.kt
@@ -2,10 +2,17 @@ package com.example.nbcamp_week4_assignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        var img_profile = findViewById<ImageView>(R.id.iv_profile)
+
+        img_profile.setImageResource(R.drawable.ms)
+        img_profile.clipToOutline = true
+
+
     }
 }
