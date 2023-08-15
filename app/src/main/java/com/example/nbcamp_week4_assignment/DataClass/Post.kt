b7cd6c1a40c44c9083998d6feabe3a4163e14ac9
추민수 <devsoftychoo@gmail.com>
2023-08-15 22:56:56 +0900

    [Feat] : DataClass 및 profile 리스트 뷰 구현완료
    item을 만들어 List의 형식 지정 후 기본 포스트값 넣어놓음
    DataClass(Post)와 모든기능 구현완료
    DataClass(User)은 데이터 클래스와 초반 초기화 값만 넣어놓음

---
+++ app/src/main/java/com/example/nbcamp_week4_assignment/DataClass/Post.kt
@@ -0,0 +1,6 @@
+package com.example.nbcamp_week4_assignment.DataClass
+
+import android.graphics.drawable.Drawable
+
+data class Post(val user: String, val img: String, val profile: String, val content: String) {
+}
