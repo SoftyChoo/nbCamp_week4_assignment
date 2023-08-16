46b93ad44cbc8daea814eff05594993231cc2bdf
leedaeul <ouowinnie@gmail.com>
2023-08-16 20:30:37 +0900

    edit profile set gallery

--- app/src/main/java/com/example/nbcamp_week4_assignment/EditProfileActivity.kt
+++ app/src/main/java/com/example/nbcamp_week4_assignment/EditProfileActivity.kt
@@ -1,11 +1,62 @@
 package com.example.nbcamp_week4_assignment
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import android.Manifest
+import android.widget.Button
+import android.widget.ImageButton
 
 class EditProfileActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private lateinit var button: Button
+    private var imageUri: Uri? = null
+
+    // 갤러리 open
+    private val requestPermissionLauncher: ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                openGallery()
+            }
+        }
+    // 가져온 사진 보여주기
+    private val pickImageLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data: Intent? = result.data
+                data?.data?.let {
+                    imageUri = it
+                    imageView.setImageURI(imageUri)
+                }
+            }
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+
+        title = "Edit Profile"
+        imageView = findViewById(R.id.imgProfile)
+        imageView.clipToOutline = true
+        button = findViewById(R.id.btnProfile)
+        button.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                openGallery()
+                button.text = null
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }
+    }
+    private fun openGallery() {
+        val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        pickImageLauncher.launch(gallery)
     }
 }
