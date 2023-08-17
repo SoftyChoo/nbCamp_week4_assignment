026a296046ac641bc6e1958195286afd82fca089
추민수 <devsoftychoo@gmail.com>
2023-08-17 18:04:48 +0900

    [DOCS] : 오타수정

--- app/src/main/java/com/example/nbcamp_week4_assignment/Object/UserObject.kt
+++ app/src/main/java/com/example/nbcamp_week4_assignment/Object/UserObject.kt
@@ -6,7 +6,7 @@ import com.example.nbcamp_week4_assignment.R
 
 object UserObject {
     var users = arrayListOf(
-        User("softychoo", "추민수", "000000",R.drawable.ms.toString()),
+        User("@softychoo", "추민수", "000000",R.drawable.ms.toString()),
         User("@jh4016", "이진혁", "111111",R.drawable.jh.toString()),
         User("@daeulzzang", "이다을", "222222",R.drawable.de.toString()),
         User("@hyyyjunk", "김현준", "333333",R.drawable.hj.toString()),
