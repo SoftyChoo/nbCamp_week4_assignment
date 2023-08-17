f3f6ca46c8c199c720ac76829d2a188097f448fa
추민수 <devsoftychoo@gmail.com>
2023-08-17 12:43:33 +0900

    [Feat] : Click시 개인 포스트 기능 구현 완료
    Recycler View Adaptor내에서 클릭 가능하게 구현완료했습니다.
    &
    RecyclerView 클릭 시 개인프로필로 넘어가면서 TextView값을 받아와서 이름에 따라 Main에서 모든포스트가 보이던 Listview를 ProfilePage에선 개인의 포스트만 보이게 구현하였습니다:)

--- app/src/main/java/com/example/nbcamp_week4_assignment/ProfileActivity.kt
+++ app/src/main/java/com/example/nbcamp_week4_assignment/ProfileActivity.kt
@@ -6,8 +6,10 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.ListView
+import android.widget.TextView
 import com.example.nbcamp_week4_assignment.Adaptor.PostAdaptor
 import com.example.nbcamp_week4_assignment.DataClass.Post
+import com.example.nbcamp_week4_assignment.Object.PostObject
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -17,6 +19,7 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
         val img_profile = findViewById<ImageView>(R.id.iv_profile)
         val btnEdit = findViewById<Button>(R.id.btn_edit_profile)
+        val profileName = findViewById<TextView>(R.id.tv_profile_name)
 
         img_profile.setImageResource(R.drawable.ms)//임시로
 
@@ -27,6 +30,18 @@ class ProfileActivity : AppCompatActivity() {
             val intenttoEditProfile = Intent(this, EditProfileActivity::class.java)
             startActivity(intenttoEditProfile)
         }
+        //Object에서 리스트를 받아옴
+        var personalList = arrayListOf<Post>(
+
+        )
+
+
+        personalList = PostObject.personalPost(profileName.text.toString(),personalList)
+        val personalPost = findViewById<ListView>(R.id.lv_post)
+
+        val postAdaptor = PostAdaptor(this, personalList)
+        personalPost.adapter = postAdaptor
+
 
 
 //        val postListView = findViewById<ListView>(R.id.lv_post)
--- app/src/main/java/com/example/nbcamp_week4_assignment/SignInActivity.kt
+++ app/src/main/java/com/example/nbcamp_week4_assignment/SignInActivity.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import com.example.nbcamp_week4_assignment.DataClass.Post
 import com.example.nbcamp_week4_assignment.DataClass.User
 
 class SignInActivity : AppCompatActivity() {
@@ -25,11 +24,11 @@ class SignInActivity : AppCompatActivity() {
         val yh = User("user4","양윤혁","444444")
 
         btn1.setOnClickListener {//로그인
-            if (Pass.text.toString().isNotEmpty() && ID.text.toString().isNotEmpty()) {
+//            if (Pass.text.toString().isNotEmpty() && ID.text.toString().isNotEmpty()) {
                 val intentMain = Intent(this, MainActivity::class.java)
                 startActivity(intentMain)
                 overridePendingTransition(android.R.anim.slide_in_left,android.R.anim.slide_out_right)
-            }
+//            }
         }
         btn2.setOnClickListener {//회원가입
             val intent = Intent(this, SignUpActivity::class.java)
