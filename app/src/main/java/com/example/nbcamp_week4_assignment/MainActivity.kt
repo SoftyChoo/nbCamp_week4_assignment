0f75707f09163ac2fbb907946de6bfe8a1b68dcb
추민수 <devsoftychoo@gmail.com>
2023-08-17 11:06:59 +0900

    [Feat] : cardView & Editprofile btn

--- app/src/main/java/com/example/nbcamp_week4_assignment/MainActivity.kt
+++ app/src/main/java/com/example/nbcamp_week4_assignment/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.nbcamp_week4_assignment
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -16,18 +17,6 @@ import com.example.nbcamp_week4_assignment.Object.PostObject
 
 class MainActivity : AppCompatActivity() {
 
-//    var postList = arrayListOf<Post>(
-//        Post("softyhoo","추민수", "post_ms1", "ms", "동명동 나들이 ~"),
-//        Post("yh","양윤혁", "post_yh1", "yh", "행복했던 도쿄여행"),
-//        Post("de","이다을", "post_de1", "de", "칠리치즈맛만 3번 먹은날 ㅋㅋㅋ"),
-//
-//
-//        Post("softychoo","추민수", "post_ms2", "ms", "오랜만에 일본여행!"),
-//        Post("yh","양윤혁", "post_yh2", "yh", "떨렸던 머리 올리기.."),
-//        Post("de","이다을", "post_de2", "de", "왜 다들 여기다가 보관하시는 거에요? \uD83D\uDE2E"),
-//
-//    )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,6 +24,8 @@ class MainActivity : AppCompatActivity() {
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+
+
 
         //Object에서 리스트를 받아옴
         val postList = PostObject.readPost()
@@ -46,6 +37,7 @@ class MainActivity : AppCompatActivity() {
 
         //메인화면의 User목록을 InstagramStory 처럼 가로로 나타냄(Recyclerview)
         val postRecyclerView = findViewById<RecyclerView>(R.id.rv_profile_main)
+
         //RecyclerView 가로로 변환해주는 코드
         postRecyclerView.layoutManager =LinearLayoutManager(this).also { it.orientation = LinearLayoutManager.HORIZONTAL }
         val adapter = ProfileAdaptor(this, postList)
@@ -57,11 +49,5 @@ class MainActivity : AppCompatActivity() {
         supportActionBar!!.setDisplayShowTitleEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
-
-//        postListView.setOnClickListener {
-//            //Toast.makeText(this, "${postListView}", Toast.LENGTH_SHORT).show()
-//            val intentToProfilePage = Intent(this,ProfileActivity::class.java)
-//            startActivity(intentToProfilePage)
-//        }
     }
 }
--- app/src/main/java/com/example/nbcamp_week4_assignment/Object/PostObject.kt
+++ app/src/main/java/com/example/nbcamp_week4_assignment/Object/PostObject.kt
@@ -8,12 +8,14 @@ object PostObject {
         Post("@yh", "양윤혁", "post_yh1", "yh", "행복했던 도쿄여행"),
         Post("@de", "이다을", "post_de1", "de", "칠리치즈맛만 3번 먹은날 ㅋㅋㅋ"),
         Post("@jh", "이진혁", "post_jh1", "jh", "댕댕이 숙면"),
+        Post("@hj", "김현준", "post_hj1", "hj", "맥주 한잔~"),
 
 
         Post("@softychoo", "추민수", "post_ms2", "ms", "오랜만에 일본여행!\uD83D\uDEE9️"),
         Post("@yh", "양윤혁", "post_yh2", "yh", "떨렸던 머리 올리기.."),
         Post("@de", "이다을", "post_de2", "de", "왜 다들 여기다가 보관하시는 거에요? \uD83D\uDE2E"),
-        Post("@jh", "이진혁", "post_jh2", "jh", "사촌 조카\uD83D\uDE42")
+        Post("@jh", "이진혁", "post_jh2", "jh", "사촌 조카\uD83D\uDE42"),
+        Post("@hj", "김현준", "post_hj2", "hj", "횡령산에서"),
     )
     var personalPostList = arrayListOf<Post>()
 
