a03e456f066cdc4c729cfc178286168c8a11e391
Ethan <fanfirst4@naver.com>
2023-08-18 19:39:57 +0900

    [Feat] : SignInPage KeyEvent 뒤로가기구현

--- app/src/main/java/com/example/nbcamp_week4_assignment/SignInActivity.kt
+++ app/src/main/java/com/example/nbcamp_week4_assignment/SignInActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -11,6 +12,8 @@ import com.example.nbcamp_week4_assignment.DataClass.User
 import com.example.nbcamp_week4_assignment.Object.UserObject
 
 class SignInActivity : AppCompatActivity() {
+
+    var initTime = 0L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
@@ -37,5 +40,16 @@ class SignInActivity : AppCompatActivity() {
             startActivity(intent)
             overridePendingTransition(android.R.anim.slide_in_left,android.R.anim.slide_out_right)
         }
+
+    }
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if(keyCode === KeyEvent.KEYCODE_BACK) {
+            if(System.currentTimeMillis() - initTime > 3000) {
+                Toast.makeText(this, getString(R.string.key), Toast.LENGTH_SHORT).show()
+                initTime = System.currentTimeMillis()
+                return true
+            }
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
